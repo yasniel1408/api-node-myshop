83608dfaea3cfcacd2005f6af7cd6b3715e8f76d
Yasniel <yasnielfajardoegues1408@gmail.com>
2021-11-14 14:29:55 -0500

    quit swagger

--- index.ts
+++ index.ts
@@ -5,12 +5,8 @@ import express from 'express';
 import helmet from 'helmet';
 import debug from 'debug';
 import * as http from 'http';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
 import Route from './src/common/routes';
 import router from './router';
-
-import swagger from './swagger.json';
 
 const log: debug.IDebugger = debug('index:app-server');
 
@@ -26,8 +22,6 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-const swaggerDocument = swaggerJSDoc(swagger);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
 
 const messageExpressServer = `Server is listening on ${port}`;
 
